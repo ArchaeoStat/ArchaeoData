69afaf0b60f8448ecdd3e603e3e3ec145abc1d1e
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-05-12 21:01:30 +0200

    Fix description

--- R/ArchaeoData-package.R
+++ R/ArchaeoData-package.R
@@ -1,24 +1,8 @@
 #' @details
 #' \tabular{ll}{
-#'  **Package:** \tab ArchaeoData \cr
-#'  **Type:** \tab Package \cr
 #'  **Version:** \tab 0.1.0 \cr
 #'  **License:** \tab GPL-3 \cr
 #' }
-#'
-#' @author
-#' **Full list of authors and contributors** (alphabetic order)
-#'
-#' \tabular{ll}{
-#'   Thomas S. Dye \tab *University of Hawai'i at Manoa, USA* \cr
-#'   Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
-#'   Anne Philippe \tab *Université de Nantes, France* \cr
-#' }
-#'
-#' **Package maintainer**
-#'
-#'  Anne Philippe\cr
-#'  \email{anne.philippe@@univ-nantes.fr}
 #'
 #'  Laboratoire de Mathématiques Jean Leray (UMR 6629)\cr
 #'  2, rue de la Houssinière\cr
